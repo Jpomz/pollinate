3b65fb331e67233d32be19b2e8f01da797c848e9
Justin Pomeranz <jfpomeranz@gmail.com>
2018-05-05 07:50:45 -0600

    clip sp_df to years after 1980

--- get_bird_data.R
+++ get_bird_data.R
@@ -13,7 +13,9 @@ get_bird_data <- function(taxon_name, limit = 500){
   tax_occ_df <- tax_occ_df[, colnames(tax_occ_df)
                            %in% colnames_string]
   tax_occ_df <- tax_occ_df[tax_occ_df$countryCode=="US", ]
-  tax_occ_df <- tax_occ_df[,-7]
+  tax_occ_df <- tax_occ_df[,
+                  which(colnames(tax_occ_df) %in%
+                          "countryCode")]
   names(tax_occ_df)[1] <- "species"
   tax_occ_df <- tax_occ_df[,c(1,3,2,5,6,4)]
   return(tax_occ_df)
