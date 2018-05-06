1748db55cf47f389a03f18afb7b9b50548fdff74
Justin Pomeranz <jfpomeranz@gmail.com>
2018-05-06 09:53:46 -0600

    add graphs and stats

--- pollen_bird_stats.R
+++ pollen_bird_stats.R
@@ -21,5 +21,25 @@ model <- glm(bird ~pollen + box,
              family=binomial(link='logit'),
              data= result_binom)
 summary(model)
-
 anova(model, test="Chisq")
+
+newdat <- data.frame(pollen = 
+                       rep(seq(min(result_binom$pollen),
+                                  max(result_binom$pollen),
+                                  len = 100), each = 3))
+newdat$box <- rep(unique(result_binom$box))
+newdat$fit <- predict(model, newdata = newdat,
+                       type = "response")
+#newdat$fit <- model$family$linkinv(newdat$fit)
+
+
+ggplot(result_binom, aes(x = pollen,
+                                           y = bird,
+                         color = box)) +
+  geom_point() +
+  geom_line(data = newdat, aes(y=fit), size = 1) +
+  scale_color_brewer(palette = 'Accent', labels = c('Southeast', 'Mideast','Northeast'), name = 'Region') +
+  labs(x = 'Pollen searches', y = 'Probability of bird sighting') +
+  theme_bw()
+
+ggsave("Figure/Logistic_regression.png")
