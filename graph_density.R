fc9064f18858c2065a223266e7aad6bbe548404f
halichoeres02 <salamon.mathilde@courrier.uqam.ca>
2018-05-06 09:26:59 -0400

    updated graph

--- graph_density.R
+++ graph_density.R
@@ -3,30 +3,35 @@ library(gridExtra)
 library(dplyr)
 library(tidyr)
 
-master_table <- read.csv(test_data.csv, header = T, sep =";")
+master_table <- read.csv(obs_per_unit.csv, header = T, sep =";")
 
 dens_plot <- function(master_table,box1,box2,box3, year){
   
-    table_1y <- filter(master_table, year = year)%>%
-    unite("date",c("year", "month", "day"), sep="-")
+    table_1y <- filter(master_table, year == year)%>%
+    select("date","month", sep="-")
     
-    state1 <- filter(table_1y , box = box1)
-    state2 <- filter(table_1y , box = box2)
-    state3 <- filter(table_1y , box = box3)
+    state1 <- filter(table_1y , box == 1)%>%
+    group_by(species, pollen, date) %>%
+    summarise(n=n()) %>%
+    select(date, pollen, n) %>%
+    gather(species, count, pollen:n)
+      
+    state2 <- filter(table_1y , box == box2)%>%count(species, date)
+    state3 <- filter(table_1y , box == box3)%>%count(species, date)
   
-    p1 <- ggplot(state1, aes(date, count), colour = species)+
-    geom_density()+
-    scale_x_date(date_breaks = "1 month", date_labels = "%M")+
+    (p1 <- ggplot(state1, aes(x= date, y = count, color = species))+
+    geom_line()+
+    #scale_x_date(date_breaks = "1 month", date_labels = "%M")+
     theme_bw()+
-    panel.grid.major = element_blank()+
+    theme(panel.grid.major = element_blank())+
     xlab("Time")+
-    ylab("Density")
+    ylab("Density"))
 
     p2 <- ggplot(state2, aes(date, count), colour = species)+
     geom_density()+
     scale_x_date(date_breaks = "1 month", date_labels = "%M")+
     theme_bw()+
-    panel.grid.major = element_blank()
+    theme(panel.grid.major = element_blank())+
     xlab("Time")+
     ylab("Density")
   
@@ -34,7 +39,7 @@ dens_plot <- function(master_table,box1,box2,box3, year){
     geom_density()+
     scale_x_date(date_breaks = "1 month", date_labels = "%M")+
     theme_bw()+
-    panel.grid.major = element_blank()+
+    theme(panel.grid.major = element_blank())+
     xlab("Time")+
     ylab("Density")
     
