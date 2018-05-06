8f70ad49bdb06a13cb25d1796a93704205f74296
halichoeres02 <salamon.mathilde@courrier.uqam.ca>
2018-05-06 10:59:22 -0400

    density_plot

--- graph_density.R
+++ graph_density.R
@@ -4,47 +4,57 @@ library(dplyr)
 library(tidyr)
 
 
-master_table <- read.csv(obs_per_unit.csv, header = T, sep =";")
+obs <- read.csv("Data/result_table.csv", header = T)
 
-dens_plot <- function(master_table,box1,box2,box3, year){
+dens_plot <- function(master_table, yr){
   
-    table_1y <- filter(master_table, year == year)%>%
-    select("date","month", sep="-")
+    table_1y <- filter(master_table, year == yr, box == 1 )%>%
+    select("box","month", "pollen","bird")%>%
+    gather(species, count, pollen:bird)
     
-    state1 <- filter(table_1y , box == 1)%>%
-    group_by(species, pollen, date) %>%
-    summarise(n=n()) %>%
-    select(date, pollen, n) %>%
-    gather(species, count, pollen:n)
-      
-    state2 <- filter(table_1y , box == box2)%>%count(species, date)
-    state3 <- filter(table_1y , box == box3)%>%count(species, date)
+    table_2y <- filter(master_table, year == yr, box ==2)%>%
+      select("box","month", "pollen","bird")%>%
+      gather(species, count, pollen:bird)
+    
+    table_3y <- filter(master_table, year == yr, box ==3 )%>%
+      select("box","month", "pollen","bird")%>%
+      gather(species, count, pollen:bird)
   
-    (p1 <- ggplot(state1, aes(x= date, y = count, color = species))+
-    geom_line()+
-    #scale_x_date(date_breaks = "1 month", date_labels = "%M")+
+    p1 <- ggplot(table_1y, aes(x= month, y = count, color = species, fill= species))+
+    geom_area(alpha=0.4)+
     theme_bw()+
     theme(panel.grid.major = element_blank())+
-    xlab("Time")+
-    ylab("Density"))
+    scale_x_continuous("months", breaks=1:12 , labels=c("J", "F","M","A","M","J","JL","A","S","O","N","D"))+ 
+    xlab("Month")+
+    ylab("Observations") +
+    scale_fill_manual(values = c("red","green")) +
+    scale_color_manual(values = c("darkred","darkgreen")) +
+    ggtitle("South")
 
-    p2 <- ggplot(state2, aes(date, count), colour = species)+
-    geom_density()+
-    scale_x_date(date_breaks = "1 month", date_labels = "%M")+
-    theme_bw()+
-    theme(panel.grid.major = element_blank())+
-    xlab("Time")+
-    ylab("Density")
+    p2 <- ggplot(table_2y, aes(x= month, y = count, color = species, fill= species))+
+        geom_area(alpha=0.4)+
+        theme_bw()+
+        theme(panel.grid.major = element_blank())+
+        scale_x_continuous("months", breaks=1:12 , labels=c("J", "F","M","A","M","J","JL","A","S","O","N","D"))+ 
+        xlab("Month")+
+        ylab("Observations") +
+        scale_fill_manual(values = c("red","green")) +
+        scale_color_manual(values = c("darkred","darkgreen")) +
+        ggtitle("Mid-East")
   
-    p3 <- ggplot(state3, aes(date, count), colour = species)+
-    geom_density()+
-    scale_x_date(date_breaks = "1 month", date_labels = "%M")+
-    theme_bw()+
-    theme(panel.grid.major = element_blank())+
-    xlab("Time")+
-    ylab("Density")
+    p3 <- ggplot(table_3y, aes(x= month, y = count, color = species, fill= species))+
+        geom_area(alpha=0.4)+
+        theme_bw()+
+        theme(panel.grid.major = element_blank())+
+        scale_x_continuous("months", breaks=1:12 , labels=c("J", "F","M","A","M","J","JL","A","S","O","N","D"))+ 
+        xlab("Month")+
+        ylab("Observations") +
+        scale_fill_manual(values = c("red","green")) +
+        scale_color_manual(values = c("darkred","darkgreen")) +
+        ggtitle("Northeast")
     
-    plot_boxes <- grid.arrange(p1, p2, p3, cols=2)
+    
+    plot_boxes <- grid.arrange(p1, p2, p3, top=as.character(yr))
     return(plot_boxes)
 }
 
